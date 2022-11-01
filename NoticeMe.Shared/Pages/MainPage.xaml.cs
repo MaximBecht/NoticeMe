09d2b901e7bbc675c761206d165557fd971072a6
MaximBecht <maxim.becht@gmail.com>
2022-11-01 14:02:23 +0100

    Simple navigation between pages

--- NoticeMe.Shared/Pages/MainPage.xaml.cs
+++ NoticeMe.Shared/Pages/MainPage.xaml.cs
@@ -25,11 +25,32 @@ namespace NoticeMe.Pages
             ContentFrame.Navigate(typeof(HomePage));
         }
 
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            switch (b.Name)
+            {
+                case "NavigateLeaderBoardBtn": break;
+                case "NavigateHomeBtn": ContentFrame.Navigate(typeof(HomePage)); break;
+                case "NavigateSettingsBtn": ContentFrame.Navigate(typeof(SettingsPage)); break;
+            }
+            //more logic to do here...
+        }
+
+
+        /// <summary>
+        /// Just for testing RestApi
+        /// </summary>
         public async void TestRestApiButton()
         {
             await restApiTests.ProcessRepositories();
         }
 
+
+        /// <summary>
+        /// Just as a reminder of possibilities
+        /// </summary>
+        /// <returns></returns>
         public static string CurrentTheme()
         {
             var isDark = Application.Current.RequestedTheme == ApplicationTheme.Dark;
