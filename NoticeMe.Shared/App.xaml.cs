32c0d08646387e227be0f5c743803323ef1cd041
Maxim Becht <93044169+MaximBecht@users.noreply.github.com>
2023-01-28 22:09:35 +0100

    Quick fix: Microsoft.Extensions.Logging was removed automatically

--- NoticeMe.Shared/App.xaml.cs
+++ NoticeMe.Shared/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using NoticeMe.Pages;
